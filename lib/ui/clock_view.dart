63b7af3cf00d2956ce3c4b523ff1d99fe7023d6f
vicky-prog <johnvick281997@gmail.com>
2022-08-02 21:25:04 +0530

    update

--- lib/ui/clock_view.dart
+++ lib/ui/clock_view.dart
@@ -32,6 +32,7 @@ class _ClockWidgetState extends State<ClockWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.blueGrey,
       height: 200,
       width: 200,
       // color: HexColor("#8080cc"),
@@ -66,23 +67,46 @@ class ClockPainter extends CustomPainter {
     var secondHand = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 3
+      ..strokeCap = StrokeCap.round
       ..color = Colors.white;
+
+    /// Clock Minutes Hand Paint
+    var minuteHand = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3
+      ..strokeCap = StrokeCap.round
+      ..color = Colors.purple;
 
     /// Small Circle
     var smallCircle = Paint()..color = Colors.white;
 
     /// Draw Clock Circle
-    canvas.drawCircle(centerPoint, radius, brush);
+    canvas.drawCircle(centerPoint, radius - size.width * 0.10, brush);
 
     /// Draw Small Circle
     canvas.drawCircle(centerPoint, 5, smallCircle);
 
     /// Draw Seconds Hand
-    var secHandX = centerX + 70 * cos(dateTime.second * 6 * pi / 360);
-    var secHandY = centerX + 70 * sin(dateTime.second * 6 * pi / 360);
+    var secHandX = centerX + 70 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 70 * sin(dateTime.second * 6 * pi / 180);
     print(secHandX);
     print(secHandY);
     canvas.drawLine(centerPoint, Offset(secHandX, secHandY), secondHand);
+
+    /// Draw Minutes Hand
+    var minHandX = centerX + 45 * cos(dateTime.minute * 8 * pi / 180);
+    var minHandY = centerX + 45 * sin(dateTime.minute * 8 * pi / 180);
+    print(minHandX);
+    print(minHandY);
+    _drawOuterLine(canvas, minuteHand);
+    // canvas.drawLine(centerPoint, Offset(minHandX, minHandY), minuteHand);
+  }
+
+  _drawOuterLine(Canvas canvas, Paint minuteHand) {
+    canvas.drawLine(const Offset(0, 0), Offset(200, 0), minuteHand);
+    canvas.drawLine(const Offset(0, 200), Offset(200, 200), minuteHand);
+    canvas.drawLine(const Offset(200, 200), Offset(200, 0), minuteHand);
+    canvas.drawLine(const Offset(0, 0), Offset(0, 200), minuteHand);
   }
 
   @override
